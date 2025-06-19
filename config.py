585a4c739d2ecaf8c9e355da79d0925e11c2e369
ROCKIND <136447110+ROCKIND@users.noreply.github.com>
2025-06-19 09:48:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,40 @@
+
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+
+DS_API_ID = int(environ.get("DS_API_ID", "16536417"))
+DS_API_HASH = environ.get("DS_API_HASH", "f6e58a549da642d7b765744a2f82c6d9")
+DS_BOT_TOKEN = environ.get("DS_BOT_TOKEN", "7016777070:AAFlFW1ELdwUP36MV0zCyy43Dlv9iHSz5HI")
+DS_BOT_USERNAME = environ.get("DS_BOT_USERNAME", "DS_TESTER_BOT") # bot username without @
+DS_LOG_CHANNEL = int(environ.get("DS_LOG_CHANNEL", "-1002609521633"))
+DS_STICKER = environ.get("DS_STICKER", "CAACAgIAAxkBAAKxemhQQxjjYapRz_MnIaOVoeX2QwaZAAL_TAACmcgQSd6zWuf0AdV3NgQ")
+DS_PIC = environ.get('DS_PIC', 'https://i.ibb.co/ch3n9wsf/x.jpg')
+
+# Database Channel For Text Or Caption Store 
+DS_DESI_FILE_CHANNEL = int(environ.get("DS_DESI_FILE_CHANNEL", "-1002499774859"))
+DS_VIDESI_FILE_CHANNEL = int(environ.get("DS_VIDESI_FILE_CHANNEL", "-1002832361772"))
+FREE_LIMIT_DESI = 10
+FREE_LIMIT_VIDESI = 3
+PREMIUM_LIMIT_DESI = 40
+PREMIUM_LIMIT_VIDESI = 15
+
+# Bot Admins
+DS_ADMINS = int(environ.get("DS_ADMINS", "1562935405"))
+
+# Mongodb Database 
+DS_DB_URI = environ.get("DS_DB_URI", "mongodb+srv://Botmaster:Botmaster@cluster08283746473883.mfjsvds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster08283746473883")
+DS_DB_NAME = environ.get("DS_DB_NAME", "achatvjbot")
+
+# True Or False
+DS_FORWARD = bool(environ.get("DS_FORWARD", False))
+
+# Force subscribe channel 
+DS_AUTH_CHANNEL = int(environ.get('DS_AUTH_CHANNEL', '-1002609521633')) # give your force subscribe channel id here else leave it blank
+
+# Verification Variables
+DS_API = environ.get("DS_API", "f12bdd3c5c118fdc7ab6dd26bbf360968923f379") # shortlink api
+DS_URL = environ.get("DS_URL", "tnshort.net") # shortlink domain without https://
+DS_VERIFY_TUTORIAL = environ.get("DS_VERIFY_TUTORIAL", "https://t.me/Robo_5_0/44") # how to open link 
+DS_VERIFICATION = bool(environ.get("DS_VERIFICATION", True)) # set True Or False and make sure spelling is correct and first letter capital.
