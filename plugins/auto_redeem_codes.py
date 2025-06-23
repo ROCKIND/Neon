57215a25124c97a9d96281ca68f41418f9fc3ffd
ROCKIND <136447110+ROCKIND@users.noreply.github.com>
2025-06-23 16:39:47 +0530

    Update auto_redeem_codes.py

--- plugins/auto_redeem_codes.py
+++ plugins/auto_redeem_codes.py
@@ -5,7 +5,7 @@ import string
 from datetime import datetime, timedelta
 import pytz
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import AUTO_REDEEM_CODE, DURATION_OF_PREMIUM, POST_DELETE_TIME, DS_AUTH_CHANNEL, DS_BOT_USERNAME, DS_LOG_CHANNEL, VALID_REDEEM_CODES, REDEEM_CODES_TRACKER
+from config import AUTO_REDEEM_CODE, DURATION_OF_PREMIUM, POST_DELETE_TIME, DS_AUTH_CHANNEL, DS_BOT_USERNAME, DS_LOG_CHANNEL, VALID_REDEEM_CODES
 from .scheduler import scheduler
 from utils import generate_code
 
